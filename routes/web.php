e89a23c59e69563d91a78811ee9ee3e6538c468d
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-02-21 14:37:13 +0700

    modified:   composer.json 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user/{name?}', function ($name='John') { 
     return 'Nama saya '.$name; 
 });
+
+Route::get('/user/profile', function () { 
+// 
+})->name('profile'); 
+Route::get( 
+    '/user/profile', 
+    [UserProfileController::class, 'show'] 
+)->name('profile'); 
+// Generating URLs... 
+$url = route('profile'); 
+// Generating Redirects... 
+return redirect()->route('profile');
