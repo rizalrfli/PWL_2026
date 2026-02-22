ce9533a5e02e1d82703216858086242c5e261dec
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-02-22 10:51:44 +0700

    Resourcess controller

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 
+Route::resource('photos', PhotoController::class)->only([ 
+'index', 'show' 
+]); 
+Route::resource('photos', PhotoController::class)->except([ 
+'create', 'store', 'update', 'destroy' 
+]);
+Route::resource('photos', PhotoController::class); 
 Route::get('/hello', [WelcomeController::class,'hello']);
 Route::get('/', [PageController::class, 'index']);
 Route::get('/about', [PageController::class, 'about']);
